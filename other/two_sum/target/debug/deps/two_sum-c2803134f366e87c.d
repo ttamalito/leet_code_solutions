77fb95377332f9e7c74805859af541b70000ec05
Jose Gonzalez <coding.tamalito@gmail.com>
2024-03-05 18:20:40 +0100

    remove target directory

--- other/two_sum/target/debug/deps/two_sum-c2803134f366e87c.d
+++
@@ -1,5 +0,0 @@
-/home/jogo/Documents/leetCode/leet_code_solutions/other/two_sum/target/debug/deps/two_sum-c2803134f366e87c: src/main.rs
-
-/home/jogo/Documents/leetCode/leet_code_solutions/other/two_sum/target/debug/deps/two_sum-c2803134f366e87c.d: src/main.rs
-
-src/main.rs:
--- other/two_sum/target/debug/two_sum.d
+++
@@ -1,1 +0,0 @@
-/home/jogo/Documents/leetCode/leet_code_solutions/other/two_sum/target/debug/two_sum: /home/jogo/Documents/leetCode/leet_code_solutions/other/two_sum/src/main.rs
